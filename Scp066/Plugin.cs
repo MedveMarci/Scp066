88ff5915e6d9178171106525015d5d4d2cc656b9
MedveMarci <martonmende@gmail.com>
2026-01-17 15:54:34 +0100

    Version bump

--- Scp066/Plugin.cs
+++ Scp066/Plugin.cs
@@ -17,7 +17,7 @@ public class Scp066 : Plugin<Config>
         "Adds SCP-066, the noise maker, as a custom role with unique abilities and features.";
 
     public override string Author => "RisottoMan, LabApi version: MedveMarci";
-    public override Version Version => new(1, 1, 2);
+    public override Version Version => new(1, 1, 3);
     public override Version RequiredApiVersion { get; } = new(LabApiProperties.CompiledVersion);
     public static Scp066 Singleton { get; private set; }
     private Scp066Role Role { get; set; }
